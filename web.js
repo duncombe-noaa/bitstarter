cdb536214c2969bd63fb62501da1bfcdb2f05f08
duncombe <christopher.duncombe.rae@gmail.com>
2013-07-08 15:54:33 +0000

    modified web.js to read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,19 @@
+
+var fs = require('fs');
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var buffer = fs.readFileSync('index.html');
+
+var textstring = buffer.toString('utf8');
+
+// console.log(textstring);
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  // response.send('Hello World 2!');
+  response.send(textstring);
 });
 
 var port = process.env.PORT || 5000;
