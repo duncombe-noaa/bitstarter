3121f365cf3ab8a8a1e45467dec6bb3abf95ca1f
Chris Duncombe Rae <christopher.duncombe.rae@gmail.com>
2013-07-09 10:14:17 -0400

    Editing grader.js script

--- grader.js
+++ grader.js
@@ -24,6 +24,9 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
+
+var URLFILE_DEFAULT = "http://localhost/index.html";
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -65,7 +68,9 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url_file>', 'URL path', clone(assertFileExists), URLFILE_DEFAULT)
         .parse(process.argv);
+    var urlFile = program.url;
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
